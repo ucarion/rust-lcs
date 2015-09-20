615978c214c8df0a5c712da94a37d2ae3197a67d
Ulysse Carion <ulysse@ulysse.io>
2015-09-19 18:57:37 -0700

    Add a function for getting the lcs

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,27 @@ impl LcsTable {
 
         LcsTable { lengths: lengths }
     }
+
+    pub fn longest_common_subsequence<'a, T: Eq>(&self, a: &'a [T], b: &'a [T]) -> Vec<&'a T> {
+        self.backtrack(a, b, a.len(), b.len())
+    }
+
+    fn backtrack<'a, T: Eq>(&self, a: &'a [T], b: &'a [T], i: usize, j: usize) -> Vec<&'a T> {
+        if i == 0 || j == 0 {
+            vec![]
+        } else if a[i - 1] == b[j - 1] {
+            let mut prefix_lcs = self.backtrack(a, b, i - 1, j - 1);
+            prefix_lcs.push(&a[i - 1]);
+
+            prefix_lcs
+        } else {
+            if self.lengths[i][j - 1] > self.lengths[i - 1][j] {
+                self.backtrack(a, b, i, j - 1)
+            } else {
+                self.backtrack(a, b, i - 1, j)
+            }
+        }
+    }
 }
 
 #[test]
@@ -41,4 +62,13 @@ fn test_lcs_table() {
     ];
 
     assert_eq!(expected_lengths, actual_lengths);
+}
+
+#[test]
+fn test_lcs_lcs() {
+    let a: Vec<_> = "XXXaXXXbXXXc".chars().collect();
+    let b: Vec<_> = "YYaYYbYYc".chars().collect();
+
+    let lcs = LcsTable::new(&a, &b).longest_common_subsequence(&a, &b);
+    assert_eq!(vec![&'a', &'b', &'c'], lcs);
 }
