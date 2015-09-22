b0c1838e5faf7318248d6c564a3669bf708872c5
Ulysse Carion <ulysse@ulysse.io>
2015-09-21 23:36:40 -0700

    Add docs

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['lcs'] = ["impl&lt;'a, T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> + 'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='lcs/struct.LcsTable.html' title='lcs::LcsTable'>LcsTable</a>&lt;'a, T&gt;","impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='lcs/enum.DiffComponent.html' title='lcs::DiffComponent'>DiffComponent</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ lcs/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["DiffComponent",""]],"struct":[["LcsTable",""]]});
